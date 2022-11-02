e00c971ac231be999089618ee9909cfa30704f56
saidiya1021 <109952168+saidiya1021@users.noreply.github.com>
2022-11-03 01:10:07 +0530

    commit10

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+ 
   x = nums1[0:m]
   y = nums2[0:n]
   nums1=x+y
@@ -13,21 +14,30 @@ def merge_sort(myList) -> None:
         left = myList[:mid]
         right = myList[mid:]
 
+        # Recursive call on each half
         merge_sort(left)
         merge_sort(right)
+
+        # Two iterators for traversing the two halves
         i = 0
         j = 0
+        
+        # Iterator for the main list
         k = 0
         
         while i < len(left) and j < len(right):
             if left[i] <= right[j]:
-                myList[k] = left[i]
-                i += 1
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
             else:
                 myList[k] = right[j]
                 j += 1
+            # Move to the next slot
             k += 1
 
+        # For all the remaining values
         while i < len(left):
             myList[k] = left[i]
             i += 1
@@ -37,8 +47,7 @@ def merge_sort(myList) -> None:
             myList[k]=right[j]
             j += 1
             k += 1
-  return myList
-
+  return 
 
 # Do not change the following code
 nums1 = []
